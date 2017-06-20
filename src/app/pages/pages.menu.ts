9eac525af131567c070e15b19bcb5a27a72f452c
Can Altinigne <can@pcitdb44>
2017-06-20 12:06:44 +0200

    Tables are removed

--- src/app/pages/pages.menu.ts
+++ src/app/pages/pages.menu.ts
@@ -15,52 +15,6 @@ export const PAGES_MENU = [
         }
       },
       {
-        path: 'tables',
-        data: {
-          menu: {
-            title: 'general.menu.tables',
-            icon: 'ion-grid',
-            selected: false,
-            expanded: false,
-            order: 500,
-          }
-        },
-        children: [
-          {
-            path: 'basictables',
-            data: {
-              menu: {
-                title: 'general.menu.basic_tables',
-              }
-            }
-          },
-          {
-            path: 'smarttables',
-            data: {
-              menu: {
-                title: 'general.menu.smart_tables',
-              }
-            }
-          },
-          {
-            path: 'datatables',
-            data: {
-              menu: {
-                title: 'Data Tables',
-              }
-            }
-          },
-           {
-             path: 'hottables',
-             data: {
-               menu: {
-                 title: 'Hot Tables',
-               }
-             }
-           }
-        ]
-      },
-      {
         path: 'maps',
         data: {
           menu: {
