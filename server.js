60bc35259a43575d44df2b5a6dc73b8fcbf02b37
Ignacio Coterillo <ignacio.coterillo@cern.ch>
2017-07-28 17:35:01 +0200

    PROXY: First implementation of the Apiato proxy

--- server.js
+++ server.js
@@ -13,18 +13,18 @@ const api = require('./server/routes/api');
 const app = express();
 
 
-// Parsers for POST data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// Set our api routes
+app.use('/api/v1', api);
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Set our api routes
-app.use('/api/v1', api);
+// Parsers for POST data
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Catch all other routes and return the index file
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
--- server/config.js
+++ server/config.js
@@ -2,4 +2,13 @@ var config = {};
 
 config.port = process.env.PORT || 3000;
 
+var apiato = {};
+
+apiato.url = "http://localhost:5443/api/v1/*"
+apiato.user = "dummyuser"
+apiato.password = "dummypassword"
+apiato.ACL = {}
+
+config.apiato = apiato;
+
 module.exports = config;
