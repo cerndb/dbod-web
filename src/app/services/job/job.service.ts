9132f822f1da6c87414dcd735680f9f5e760d192
José Andrés Cordero Benítez <j.cordero@cern.ch>
2017-08-21 12:12:19 +0200

    JOB: Updated Job Service to use API.

--- src/app/services/job/job.service.ts
+++ src/app/services/job/job.service.ts
@@ -1,20 +1,30 @@
 import { Injectable } from '@angular/core';
 import { Http } from '@angular/http'; 
 import { Response, Headers, RequestOptions } from '@angular/http';
-import {Observable} from 'rxjs/Rx';
+import { Observable } from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class JobService {
     constructor(private http: Http) {
-         var obj;
-         this.getJobs().subscribe(data => obj=data, error => console.log(error));
+         let obj;
+         this.getJobs().subscribe(data => obj = data, error => console.log(error));
     }
 
     getJobs(): Observable<any> {
-         return this.http.get('assets/job-info.json')
-                         .map((res:any) => res.json());
+        return this.http.get('assets/job-info.json')
+            .map((res: any) => res.json());
 
-     }
+    }
+
+    getJobInfo(name: string, job: number): Observable<any> {
+        return this.http.get(`/api/v1/instance/${name}/job/${job}`)
+            .map(res => res.json());
+    }
+
+    getJobsForInstance(name: string): Observable<any> {
+        return this.http.get(`/api/v1/instance/${name}/job`)
+            .map(res => res.json());
+    }
 }
