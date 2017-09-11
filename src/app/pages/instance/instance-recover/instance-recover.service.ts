5b6eddddb7f8b223dd56d5eb5d3c5c99d4574a9b
José Andrés Cordero Benítez <j.cordero@cern.ch>
2017-09-12 01:25:49 +0200

    RECOVER: Added mocked data to test functionality.

--- src/app/pages/instance/instance-recover/instance-recover.service.ts
+++ src/app/pages/instance/instance-recover/instance-recover.service.ts
@@ -20,8 +20,52 @@ export class InstanceRecoverService {
       editable: true,
       eventLimit: true,
       events: [
-        
-      ]
+        {
+          title: 'Snapshot #1',
+          start: '2017-09-01T09:00:00',
+          color: '#005562',
+        },
+        {
+          title: 'Snapshot #2',
+          start: '2017-09-01T12:00:00',
+          color: '#005562',
+        },
+        {
+          title: 'Snapshot #3',
+          start: '2017-09-01T17:00:00',
+          color: '#005562',
+        },
+        {
+          title: 'Snapshot #1',
+          start: '2017-09-04T10:30:00',
+          color: '#005562',
+        },
+        {
+          title: 'Snapshot #1',
+          start: '2017-09-05T10:30:00',
+          color: '#005562',
+        },
+        {
+          title: 'Snapshot #1',
+          start: '2017-09-06T10:30:00',
+          color: '#005562',
+        },
+        {
+          title: 'Snapshot #2',
+          start: '2017-09-06T18:30:00',
+          color: '#005562',
+        },
+        {
+          title: 'Snapshot #1',
+          start: '2017-09-07T10:30:00',
+          color: '#005562',
+        },
+        {
+          title: 'Snapshot #1',
+          start: '2017-09-14T20:00:00',
+          color: '#005562',
+        },
+      ],
     };
   }
 }
