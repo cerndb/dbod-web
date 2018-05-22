addecb32771e55da3e70c9d470d3603bff3aa234
Ignacio Coterillo <ignacio.coterillo@cern.ch>
2018-05-22 13:28:20 +0200

    THEME: Footer

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -4,12 +4,9 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">Created with ♥ by <b><a href="https://akveo.com" target="_blank">Akveo</a></b> 2017</span>
+    <span class="created-by"><b><a href="https://home.cern" target="_blank">© CERN</a></b> 2018</span>
     <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
-      <a href="#" target="_blank" class="ion ion-social-facebook"></a>
-      <a href="#" target="_blank" class="ion ion-social-twitter"></a>
-      <a href="#" target="_blank" class="ion ion-social-linkedin"></a>
+      <a href="https://github.com/cerndb/dbod-web" target="_blank" class="ion ion-social-github"></a>
     </div>
   `,
 })
