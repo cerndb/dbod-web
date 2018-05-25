f5061d1446cb55cac47b0b750ec9832973c1d9fa
Ignacio Coterillo <ignacio.coterillo@cern.ch>
2018-05-25 16:07:38 +0200

    WIP: CERN Toolbar + icons to footer

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -5,6 +5,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./footer.component.scss'],
   template: `
     <span class="created-by"><b><a href="https://home.cern" target="_blank">© CERN</a></b> 2018</span>
+    <div class="logo-cern"></div>
+    <div class="logo-dbod"></div>
     <div class="socials">
       <a href="https://github.com/cerndb/dbod-web" target="_blank" class="ion ion-social-github"></a>
     </div>
