a361f1c2a5dad689328715405a24ddf239594dc3
José Andrés Cordero Benítez <j.cordero@cern.ch>
2018-05-29 14:15:00 +0200

    Use instance's data in metadata editor.

--- src/app/pages/instance/metadata-editor/metadata-editor.component.ts
+++ src/app/pages/instance/metadata-editor/metadata-editor.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild, ViewEncapsulation} from '@angular/core';
+import {Component, Input, ViewChild, ViewEncapsulation} from '@angular/core';
 import { JsonEditorComponent, JsonEditorOptions } from 'ang-jsoneditor';
 
 @Component({
@@ -9,16 +9,13 @@ import { JsonEditorComponent, JsonEditorOptions } from 'ang-jsoneditor';
 })
 export class MetadataEditorComponent {
   public editorOptions: JsonEditorOptions;
-  public data: any;
+  @Input() data: any;
 
   @ViewChild(JsonEditorComponent) editor: JsonEditorComponent;
 
   constructor() {
     this.editorOptions = new JsonEditorOptions()
-    this.editorOptions.modes = ['code', 'text', 'tree', 'view']; // set all allowed modes
-    //this.options.mode = 'code'; //set only one mode
-
-    this.data = {"products":[{"name":"car","product":[{"name":"honda","model":[{"id":"civic","name":"civic"},{"id":"accord","name":"accord"},{"id":"crv","name":"crv"},{"id":"pilot","name":"pilot"},{"id":"odyssey","name":"odyssey"}]}]}]}
+    this.editorOptions.modes = ['code', 'tree']; // set all allowed modes
   }
 
 }
