970aae85335a8fbcbe005d8e029eea88ff214961
José Andrés Cordero Benítez <j.cordero@cern.ch>
2018-05-30 13:02:56 +0200

    Cleanup and proper nb-card tags.

--- src/app/pages/instance/instance.module.ts
+++ src/app/pages/instance/instance.module.ts
@@ -1,19 +1,15 @@
 
-import { FileEditorComponent } from './../components/file-editor/file-editor.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { ThemeModule } from '../../@theme/theme.module';
-import { InstanceComponent } from './instance.component';
 import { InstanceService } from '../../services/instance';
-//import { AppTranslationModule } from '../../app.translation.module';
-import { RouterModule } from '@angular/router';
 import { InlineEditorModule } from '@qontu/ngx-inline-editor';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-//import { JWBootstrapSwitchModule } from 'jw-bootstrap-switch-ng2';
 import { UiSwitchModule } from 'ngx-toggle-switch';
-import {NgJsonEditorModule} from "ang-jsoneditor";
-import {MetadataEditorComponent} from "./metadata-editor/metadata-editor.component";
+import { NgJsonEditorModule } from 'ang-jsoneditor';
+import { InstanceComponent } from './instance.component';
+import { FileEditorComponent } from './../components/file-editor/file-editor.component';
+import { MetadataEditorComponent } from './metadata-editor/metadata-editor.component';
 
 @NgModule({
   imports: [
@@ -22,8 +18,6 @@ import {MetadataEditorComponent} from "./metadata-editor/metadata-editor.compone
     ThemeModule,
     InlineEditorModule,
     NgJsonEditorModule,
-    //NgbModule.forRoot(),
-    //JWBootstrapSwitchModule,
     UiSwitchModule,
   ],
   declarations: [
