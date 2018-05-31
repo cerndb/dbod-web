3cad8725a7e7f343e75844b311e3f16708f2b2b2
José Andrés Cordero Benítez <j.cordero@cern.ch>
2018-05-31 09:47:29 +0200

    Added instance-backups component as it was.

---
+++ src/app/pages/instance/instance-backups/instance-backups.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'instance-backups',
+  templateUrl: './instance-backups.component.html',
+  styleUrls: ['./instance-backups.component.scss']
+})
+export class InstanceBackups implements OnInit {
+
+  value: Date;
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+
+  }
+
+}
--- src/app/pages/instance/instance.module.ts
+++ src/app/pages/instance/instance.module.ts
@@ -14,6 +14,7 @@ import { MetadataEditorComponent } from './metadata-editor/metadata-editor.compo
 import { InstanceRecoverComponent } from './instance-recover/instance-recover.component';
 import { FullCalendarModule } from 'ng-fullcalendar';
 import { InstanceJobsComponent } from './instance-jobs/instance-jobs.component';
+import { InstanceBackups } from './instance-backups/instance-backups.component';
 
 @NgModule({
   imports: [
@@ -32,6 +33,7 @@ import { InstanceJobsComponent } from './instance-jobs/instance-jobs.component';
     MetadataEditorComponent,
     InstanceRecoverComponent,
     InstanceJobsComponent,
+    InstanceBackups,
   ],
   providers: [
     InstanceService,
