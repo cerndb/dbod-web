5e75a7c3c6a71f7c34047c51f7075f938bc1a355
Domingo Rivera <domingo.rivera.barros@cern.ch>
2018-08-10 17:41:21 +0200

    WIP: removing button components

--- src/app/services/job/job.service.ts
+++ src/app/services/job/job.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http'; 
+import { Http } from '@angular/http';
 import { Response, Headers, RequestOptions } from '@angular/http';
 import {Observable} from 'rxjs/Rx';
 import 'rxjs/add/operator/map';
